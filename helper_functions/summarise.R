7e77c006c375e4a246608e5a60389f44cb4e6b90
Tim Lucas <timcdlucas@gmail.com>
2020-01-11 23:44:17 +0000

    more placeholders.

---
+++ helper_functions/summarise.R
@@ -0,0 +1,20 @@
+
+
+
+summarise <- function(positive, examines, pred, coords){
+
+# Mae
+
+
+# correlation
+
+
+# scatter
+
+
+# spatial residuals
+
+# map of predictions 
+# not a raster because makes function very complicated.
+
+}
---
+++ models/enet/run.R
@@ -0,0 +1,23 @@
+header
+
+
+#'# elastic net
+
+#' standard elastic net models
+
+#+ setup
+
+
+#+ readdata
+
+
+#+ fitmodels1
+
+
+#+ fitmodels2
+
+
+
+#+ fitmodels3
+
+
