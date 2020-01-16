0538855e3227b998a532c0148b24dd965c3e044a
Tim Lucas <timcdlucas@gmail.com>
2020-01-16 17:03:57 +0000

    Add bart models.

--- models/bart/bart.R
+++ models/bart/bart.R
@@ -28,16 +28,16 @@ library(tidyr)
 library(ggplot2)
 library(data.table)
 
-library(malariaAtlas)
-library(raster)
 library(imputeMissings)
 library(stringr)
 library(parallel)
 library(caret)
-library(INLA)
+library(doParallel)
 
 source('../../helper_functions/summarise.R')
 source('../../helper_functions/extract_year.R')
+
+if(!dir.exists('models')) dir.create('models')
 
 
 #+ read_pr_data, eval = TRUE
@@ -45,107 +45,6 @@ source('../../helper_functions/extract_year.R')
 pr <- fread("../../../data/derived/malariaAtlas_pr.csv")
 
 
-#+ extract_covs, eval = FALSE
-
-
-dirs <- c(
-  '/home/tim/timz/mastergrids/MODIS_Global/MOD11A2_v6_LST/LST_Day/5km/Monthly',  
-  '/home/tim/timz/mastergrids/MODIS_Global/MOD11A2_v6_LST/LST_DiurnalDifference/5km/Monthly',
-  '/home/tim/timz/mastergrids/MODIS_Global/MOD11A2_v6_LST/LST_Night/5km/Monthly',
-  '/home/tim/timz/mastergrids/MODIS_Global/MCD43D6_v6_BRDF_Reflectance/EVI_v6/5km/Monthly',
-  '/home/tim/timz/mastergrids/MODIS_Global/MCD43D6_v6_BRDF_Reflectance/TCB_v6/5km/Monthly',
-  '/home/tim/timz/mastergrids/MODIS_Global/MCD43D6_v6_BRDF_Reflectance/TCW_v6/5km/Monthly',
-  '/home/tim/timz/mastergrids/Other_Global_Covariates/TemperatureSuitability/TSI_Pf_Dynamic/5km/Monthly',
-  '/home/tim/timz/mastergrids/Other_Global_Covariates/Rainfall/CHIRPS/5km/Monthly'
-)
-names1 <- c('LST_Day', 'LST_DiurnalDifference', 'LST_Night', 'EVI_v6',
-            'TCB_v6', 'TCW_v6', 'TSI_Pf_Dynamic', 'CHIRPS'
-)
-
-static <- c(
-  '/home/tim/timz/mastergrids/Other_Global_Covariates/Accessibility/Weiss/accessibility_to_cities_2015_v1.0.tif',
-  '/home/tim/timz/mastergrids/Other_Global_Covariates/Aridity_v2/5km/Synoptic/Aridity_Index_v2.Synoptic.Overall.Data.5km.mean.tif',
-  '/home/tim/timz/mastergrids/Other_Global_Covariates/Elevation/SRTM-Elevation/5km/Synoptic/SRTM_elevation.Synoptic.Overall.Data.5km.mean.tif',
-  '/home/tim/timz/mastergrids/Other_Global_Covariates/NightTimeLights/VIIRS_DNB_Annual_Clean_Composites/5km/Annual/VIIRS-DNB_Clean-Background.2016.Annual.Data.5km.mean.tif'
-)
-names2 <- c('accessibility', 'aridity', 'elevation', 'VIIRS')
-
-covs <- rep(list(rep(NA, nrow(pr))), length(dirs))
-
-for(i in seq_along(dirs)){
-  
-  
-  short_files <- list.files(dirs[[i]], pattern = '\\.tif$')
-  if(any(grepl('mean\\.5km\\.mean', short_files))){
-    short_files <- grep('mean\\.5km\\.mean', short_files, value = TRUE)
-  }
-  year_vec <- str_extract(short_files, '20..\\.') %>% gsub('\\.', '', .) %>% as.numeric
-  
-  month_vec <- str_extract(short_files, '[0-9]{4}\\.[0-9]{2}') %>% gsub('[0-9]{4}\\.', '', .) %>% as.numeric
-  
-  
-  full_files <-  list.files(dirs[[i]], pattern = '\\.tif$', full.names = TRUE)
-  if(any(grepl('mean\\.5km\\.mean', full_files))){
-    full_files <- grep('mean\\.5km\\.mean', full_files, value = TRUE)
-  }
-  
-  if(month_vec[1] != 1){
-    month_vec_old <- month_vec
-    month_vec <- c(1:(month_vec[1] - 1), month_vec)
-    year_vec <- c(rep(year_vec[1], (month_vec_old[1] - 1)), year_vec)
-    full_files <- c(rep(full_files[1], (month_vec_old[1] - 1)), full_files)
-  }
-  
-  covs[[i]] <- extract_year_month(coords = pr %>% dplyr::select(longitude, latitude),
-                                  data_year = pr$year_start,
-                                  data_month = pr$month_start,
-                                  files = full_files,
-                                  year_vec, 
-                                  month_vec)  
-}
-
-
-
-covs_df <- do.call(cbind, covs) %>% data.frame
-names(covs_df) <- names1
-
-write.csv(covs_df, 'base_tmp.csv')
-
-
-
-covs2 <- rep(list(rep(NA, nrow(pr))), length(static))
-coords <- pr %>% dplyr::select(longitude, latitude) %>% as.matrix
-for(i in seq_along(static)){
-  r <- raster(static[i])
-  covs2[[i]] <- raster::extract(r, coords)
-}
-
-covs_df2 <- do.call(cbind, covs2) %>% data.frame
-names(covs_df2) <- names2
-
-
-
-
-#+ clean_covs, eval = FALSE
-covs <- cbind(covs_df, covs_df2)
-sapply(covs, function(x) mean(is.na(x)))
-
-covs_clean <- impute(covs)
-
-#+ add_table_covs, eval = FALSE
-
-covs_clean <- cbind(covs_clean, dplyr::select(pr, year_start, month_start))
-
-#+ combine, eval = FALSE
-covs_clean <- scale(covs_clean)
-covs_clean <- as.data.frame(covs_clean)
-
-#+ write_covs, eval = FALSE
-
-write.csv(covs_clean, '../../../data/extracted_covs/base.csv')
-
-
-if(!dir.exists('models')) dir.create('models')
 
 #+ read_back_in
 
@@ -171,27 +70,33 @@ covs_clean <-
          VIIRS = log1p(VIIRS))
 
 
-#'# Base Data
+#+ setup_parallel
+
+cl <- makeForkCluster(10)
+registerDoParallel(cl)
+
+
+#'# Base Data bart
 #' ## Random CV
 
 
 #+ fit_base_random, cache = TRUE
-
-tg <- expand.grid(mtry = c(3, 8, 14), 
-                  min.node.size = c(5, 20, 50),  
-                  splitrule = 'variance')
 
 
 m_base_r <- train(y = pr$pf_pr[pr$random_holdout == 0],
                 x = covs_clean[pr$random_holdout == 0, ],
                 method = 'ranger', 
                 weights = pr$examined[pr$random_holdout == 0],
-                tuneGrid = tg,
+                tuneLength = 30,
                 metric = 'MAE',
-                trControl = trainControl(method = 'boot632', number = 1)
+                trControl = trainControl(method = 'cv', number = 3, 
+                                         search = 'random',
+                                         selectionFunction = 'oneSE')
 )
 
 save(m_base_r, file = 'models/base_r.RData')
+
+stopCluster(cl)
 
 
 #+ summary_base_random
