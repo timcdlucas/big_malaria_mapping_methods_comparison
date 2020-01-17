958789dc867aec9bfded97396ea349c48e99614b
Tim Lucas <timcdlucas@gmail.com>
2020-01-17 14:05:40 +0000

    Start messing with results.

--- results.R
+++ results.R
@@ -21,6 +21,8 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(readr)
+library(knitr)
+library(forcats)
 
 #'# Covariates
 
@@ -33,13 +35,46 @@ d <- lapply(files, read_csv)
 data <- do.call(rbind, d)
 
 
+files_big <- list.files('covariates', recursive = TRUE, pattern = '_errors.csv', full.names = TRUE)
+
+d_big <- lapply(files, read_csv)
+
+data_big <- do.call(rbind, d)
+
+
+
 #+ plots
 
 data %>% 
   filter(cv == 'random') %>% 
-  ggplot(aes(x = covariates, fill = method, y = mae)) + 
+  ggplot(aes(x = fct_reorder(covariates, mae, .fun = min), 
+                 fill = method, y = mae)) + 
     geom_bar(stat = 'identity', position = 'dodge')
 
+
+data %>% 
+  filter(cv == 'random') %>% 
+  ggplot(aes(x = method, fill = covariates, y = mae)) + 
+  geom_bar(stat = 'identity', position = 'dodge')
+
+
+
+#+ table
+
+data %>% 
+  filter(cv == 'random') %>% 
+  dplyr::select(-X1, -name, -cv, -time) %>% 
+  kable(caption = 'Table of results for varying covariates',
+        digits = 3)
+
+
+
+data %>% 
+  filter(cv == 'random') %>% 
+  arrange(method) %>% 
+  dplyr::select(-X1, -name, -cv, -time) %>% 
+  kable(caption = 'Table of results for varying covariates',
+        digits = 3)
 
 
 #+ spatial, eval = FALSE
@@ -52,6 +87,52 @@ data %>%
 
 #'# Models
 
+
+
+#+ read_model_summaries, warning = FALSE, results = 'hide', message = FALSE
+
+files <- list.files('models', recursive = TRUE, pattern = '_summary.csv', full.names = TRUE)
+
+d <- lapply(files, read_csv)
+
+data_mod <- do.call(rbind, d)
+
+data_mod <- data %>% 
+              filter(covariates == 'base') %>% 
+              rbind(data_mod)
+
+
+#+ read_error
+
+
+files_big <- list.files('models', recursive = TRUE, pattern = '_errors.csv', full.names = TRUE)
+
+d_big <- lapply(files_big, read_csv)
+
+data_big <- do.call(rbind, d)
+
+
+#+ plots_models
+
+data_mod %>% 
+  filter(cv == 'random') %>% 
+  ggplot(aes(x = fct_reorder(method, mae), y = mae)) + 
+    geom_bar(stat = 'identity', position = 'dodge')
+
+
+
+
+#+ table_mod
+
+
+
+
+data_mod %>% 
+  filter(cv == 'random') %>% 
+  arrange(mae) %>% 
+  dplyr::select(-X1, -name, -cv, -time) %>% 
+  kable(caption = 'Table of results for varying methods',
+        digits = 3)
 
 
 
