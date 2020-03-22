6ba7dfabac20267f4808ab8f74d198d91ce7632a
Tim Lucas <timcdlucas@gmail.com>
2020-03-22 20:31:32 +0000

    A model that at least compiles

--- models/spatially_varying/spatially_varying.R
+++ models/spatially_varying/spatially_varying.R
@@ -37,6 +37,7 @@ library(knitr)
 
 library (INLA)
 library(INLAutils)
+library(TMB)
 
 
 
@@ -117,89 +118,18 @@ mesh <- inla.mesh.2d(pr[, c('longitude', 'latitude')],
 
 mesh$n
 
-pr_inla <- pr
-pr_inla$pf_pos[pr_inla$random_holdout == 1] <- NA
-pr_inla$year_group <- as.numeric(cut(pr_inla$year_start, c(1999, 2005, 2010, 2015, 2030)))
-
-# Define penalised complexity priors for random field. 
-spde <- inla.spde2.pcmatern(mesh = mesh, alpha = 2, 
-                            prior.range = c(10, 0.01), prior.sigma = c(0.4, 0.01))
-
-spde_cov <- inla.spde2.pcmatern(mesh = mesh, alpha = 2, 
-                            prior.range = c(10, 0.01), prior.sigma = c(0.1, 0.01))
-
-
-field.indices <- inla.spde.make.index("field", n.mesh = mesh$n, n.group = length(unique(pr_inla$year_group)))
-
-#covar1.indices <- inla.spde.make.index("cov1", n.mesh = mesh$n, n.group = length(unique(pr_inla$year_group)))
-
-Ncovs <- 2
-cov_indices_list <-
-  lapply(seq_len(Ncovs), function(i) 
-    inla.spde.make.index(paste0("cov", i), n.spde = mesh$n))
-    
-
-Aest <- inla.spde.make.A(mesh = mesh, loc = as.matrix(pr_inla[, c('longitude', 'latitude')]),
-                         group = pr_inla$year_group)
-
-Acov_list <- 
-  lapply(seq_len(Ncovs), function(i)
-    inla.spde.make.A(mesh = mesh, loc = as.matrix(pr_inla[, c('longitude', 'latitude')]),
-                     weights = covs_clean[, i]))
-#Ac1 <- inla.spde.make.A(mesh = mesh, loc = as.matrix(pr_inla[, c('longitude', 'latitude')]),
-                         #group = pr_inla$year_group, weights = covs_clean$LST_Day)
-
-Alist <- c(Aest, Acov_list, rep(1, ncol(covs_clean) + 1))
-effectslist <- c(field = list(field.indices),
-                 cov1 = cov_indices_list,
-                 cbind(b0 = 1, covs_clean))
-
-#effectslist <- list(field = field.indices,
-#                    cov1 = covar1.indices,
-#                    cbind(b0 = 1, covs_clean))
-stk.env <- inla.stack(tag = 'estimation', ## tag
-                      data = list(pf_pos = pr_inla$pf_pos, examined = pr_inla$examined),
-                      A = Alist,  ## Projector matrix for space, fixed.
-                      effects = effectslist)
 
 
 
-#+ fit_mbg_random
+#+ setup_tmb
 
-h.spec <- list(theta=list(prior='pccor1', param=c(0, 0.9)))
-cov_ar1 <- list(theta=list(prior='pccor1', param=c(0, 0.95)))
+compile('spatially_varying_logit.cpp')
 
-hyper.rw2 <- list(prec = list(prior="pc.prec", param = c(1, 0.01)))
-hyper.country <- list(prec = list(prior="pc.prec", param = c(0.4, 0.01)))
-
-
-fixed <- paste(names(covs_clean %>% dplyr::select(-contains('start'))), collapse = ' + ')
-
-form1 <- 'pf_pos ~ b0 + 0 + '
-form2 <- 'f(field, model = spde, group = field.group, control.group = list(model="ar1", hyper=h.spec)) + '
-form3 <- 'f(year_start, model="rw2", hyper = hyper.rw2, scale.model = TRUE) + '
-#form4 <- 'f(year_startrep, model="rw2", hyper = hyper.country, scale.model = TRUE, replicate = country) + '
-formc1 <- 'f(cov1, model = spde_cov, group = cov1.group, control.group = list(model="ar1", hyper=cov_ar1)) + '
-form_spat_var <- lapply(seq_len(Ncovs), function(i)
-  paste0('f(cov', i, 
-         ', model = spde_cov, group = cov', i, 
-         '.group, control.group = list(model="ar1", hyper=cov_ar1)) + '))
-form_spat_var <- paste0(form_spat_var, collapse = '')
-
-form <- as.formula(paste(form1, form2, form3, form_spat_var, fixed))
-form <- as.formula(paste(form1, form3, form_spat_var, fixed))
+dyn.load(dynlib("spatially_varying_logit"))
 
 
 
-m1 <- inla(form, data = inla.stack.data(stk.env), 
-           family = 'binomial', 
-           Ntrials = pr_inla$examined, 
-           control.predictor = list(compute = TRUE, link = 1, A = inla.stack.A(stk.env)),
-           control.inla = list(int.strategy = 'eb', strategy = 'gaussian', adjust.weights = FALSE),
-           num.threads = 8, verbose = T)
-
-
-save(m1, file = 'models/inla1.RData')
+#+ fit_tmb
 
 
 #+ predict_base_random
--- models/spatially_varying/spatially_varying_logit.cpp
+++ models/spatially_varying/spatially_varying_logit.cpp
@@ -31,28 +31,16 @@ template <class Type>
     // Covariate pixel data
   DATA_MATRIX(x);
   
-  // two col matrix with start end indices for each shape case.
-  DATA_IARRAY(startendindex);
-  
   // Shape data. Cases and region id.
-  DATA_VECTOR(polygon_response_data);
+  DATA_VECTOR(response_data);
   DATA_VECTOR(response_sample_size);
   
-  // Use to aggreagte pixel response values to polygon level
-  DATA_VECTOR(aggregation_values);
-  
-  // ------------------------------------------------------------------------ //
-    // Likelihood and link functions
-  // ------------------------------------------------------------------------ //
-    
-    DATA_INTEGER(family);
-  DATA_INTEGER(link);
-  
+
   // ------------------------------------------------------------------------ //
     // Parameters
   // ------------------------------------------------------------------------ //
     
-    PARAMETER(intercept);
+  PARAMETER(intercept);
   PARAMETER_VECTOR(slope);
   
   DATA_SCALAR(priormean_intercept);
@@ -65,23 +53,7 @@ template <class Type>
   Type tau_gaussian = exp(log_tau_gaussian);
   Type gaussian_sd = 1 / sqrt(tau_gaussian);
   
-  // INLA defines a loggamma prior on log tau. 
-  // We evaluate a gamma prior on tau, but the parameters are 
-  // therefore the same.
-  Type prior_gamma_shape = 1;
-  Type prior_gamma_rate = 5e-05;
-  
-  PARAMETER_VECTOR(iideffect);
-  PARAMETER(iideffect_log_tau);
-  Type iideffect_tau = exp(iideffect_log_tau);
-  Type iideffect_sd = 1 / sqrt(iideffect_tau);
-  
-  Type iideffect_mean = 0.0;
-  
-  // Priors on iid random effect for polygons
-  DATA_SCALAR(prior_iideffect_sd_max);
-  DATA_SCALAR(prior_iideffect_sd_prob);
-  
+
   // spde hyperparameters
   PARAMETER(log_sigma);
   PARAMETER(log_rho);
@@ -103,10 +75,7 @@ template <class Type>
   
   // Model component flags
   DATA_INTEGER(field);
-  DATA_INTEGER(iid);
-  
-  // Number of polygons
-  int n_polygons = polygon_response_data.size();
+
   // Number of pixels
   int n_pixels = x.rows();
   
@@ -119,27 +88,6 @@ template <class Type>
     nll -= dnorm(intercept, priormean_intercept, priorsd_intercept, true);
   for (int s = 0; s < slope.size(); s++) {
     nll -= dnorm(slope[s], priormean_slope, priorsd_slope, true);
-  }
-  
-  if(iid) {
-    // Likelihood of hyperparameter of polygon iid random effect. 
-    // From https://projecteuclid.org/euclid.ss/1491465621 (Eqn 3.3)
-    Type lambda = -log(prior_iideffect_sd_prob) / prior_iideffect_sd_max;
-    Type log_pcdensity_iid = log(lambda / 2) - (3/2)*iideffect_log_tau - lambda * pow(iideffect_tau, -1/2);
-    // log(iideffect_sd) from the Jacobian
-    nll -= log_pcdensity_iid + log(iideffect_sd);
-    
-    // Likelihood of random effect for polygons
-    for(int p = 0; p < iideffect.size(); p++) {
-      nll -= dnorm(iideffect[p], iideffect_mean, iideffect_sd, true);
-    }
-  }
-  
-  // Likelihood from the gaussian prior. 
-  // log(prec) ~ loggamma
-  // prec ~ gamma
-  if(family == 0) {
-    nll -= dgamma(tau_gaussian, prior_gamma_shape, prior_gamma_rate, true);
   }
   
   if(field) {
@@ -167,8 +115,10 @@ template <class Type>
     // Likelihood from data
   // ------------------------------------------------------------------------ //
     
-    vector<Type> pixel_linear_pred(n_pixels);
+  vector<Type> pixel_linear_pred(n_pixels);
   pixel_linear_pred = intercept + x * slope;
+  pixel_linear_pred = invlogit(pixel_linear_pred);
+  
   
   if(field) {
     // Calculate field for pixel data
@@ -177,86 +127,27 @@ template <class Type>
     pixel_linear_pred += linear_pred_field.array();
   }
   
-  // recalculate startendindices to be in the form start, n
-  startendindex.col(1) = startendindex.col(1) - startendindex.col(0) + 1;
-  
-  Type polygon_response;
-  Type normalised_polygon_response;
-  Type normalisation_total;
-  Type pred_polygoncases;
-  Type pred_polygonrate;
-  Type polygon_sd;
+
+  Type response;
   vector<Type> pixel_pred;
-  vector<Type> numerator_pixels;
-  vector<Type> normalisation_pixels;
-  vector<Type> reportnormalisation(n_polygons);
-  vector<Type> reportprediction_cases(n_polygons);
-  vector<Type> reportprediction_rate(n_polygons);
-  vector<Type> reportnll(n_polygons);
-  vector<Type> reportpolygonsd(n_polygons);
-  
+  vector<Type> reportprediction_rate(n_pixels);
+  vector<Type> reportnll(n_pixels);
+
   // For each shape get pixel predictions within and aggregate to polygon level
-  for (int polygon = 0; polygon < n_polygons; polygon++) {
+  for (int polygon = 0; polygon < n_pixels; polygon++) {
     
-    // Get pixel level predictions (rate)
-    pixel_pred = pixel_linear_pred.segment(startendindex(polygon, 0), startendindex(polygon, 1)).array();
-    if(iid) {
-      pixel_pred += iideffect[polygon];
-    }
-    // Use correct link function
-    if(link == 0) {
-      pixel_pred = invlogit(pixel_pred);
-    } else if(link == 1) {
-      pixel_pred = exp(pixel_pred);
-    } else if(link == 2){
-      // Don't need to do anything, i.e. pixel_pred = pixel_pred;
-    } else {
-      error("Link function not implemented.");
-    }
+    nll -= dbinom(response_data[polygon], response_sample_size[polygon], pixel_pred[polygon], true);
+    reportnll[polygon] = -dbinom(response_data[polygon], response_sample_size[polygon], pixel_pred[polygon], true);
     
-    // Aggregate to polygon prediction
-    numerator_pixels = pixel_pred * aggregation_values.segment(startendindex(polygon, 0), startendindex(polygon, 1)).array();
-    normalisation_pixels = aggregation_values.segment(startendindex(polygon, 0), startendindex(polygon, 1));
-    normalisation_total = sum(normalisation_pixels);
-    pred_polygoncases = sum(numerator_pixels);
-    pred_polygonrate = pred_polygoncases/normalisation_total;
-    
-    reportnormalisation[polygon] = normalisation_total;
-    reportprediction_cases[polygon] = pred_polygoncases;
-    reportprediction_rate[polygon] = pred_polygonrate;
-    
-    // Use correct likelihood function
-    if(family == 0) {
-      // Scale the pixel sd to polygon level
-      polygon_sd = gaussian_sd * sqrt((normalisation_pixels * normalisation_pixels).sum()) / normalisation_total;
-      reportpolygonsd[polygon] = polygon_sd;
-      // Calculate normal likelihood in rate space
-      polygon_response = polygon_response_data(polygon);
-      normalised_polygon_response = polygon_response/normalisation_total;
-      nll -= dnorm(normalised_polygon_response, pred_polygonrate, polygon_sd, true);
-      reportnll[polygon] = -dnorm(normalised_polygon_response, pred_polygonrate, polygon_sd, true);
-    } else if(family == 1) {
-      nll -= dbinom(polygon_response_data[polygon], response_sample_size[polygon], pred_polygonrate, true);
-      reportnll[polygon] = -dbinom(polygon_response_data[polygon], response_sample_size[polygon], pred_polygonrate, true);
-    } else if(family == 2) {
-      nll -= dpois(polygon_response_data[polygon], pred_polygoncases, true);
-      reportnll[polygon] = -dpois(polygon_response_data[polygon], pred_polygoncases, true);
-    } else {
-      error("Likelihood not implemented.");
-    }
     
   }
   
-  REPORT(reportprediction_cases);
   REPORT(reportprediction_rate);
-  REPORT(reportnormalisation);
   REPORT(reportnll);
-  REPORT(polygon_response_data);
+  REPORT(response_data);
   REPORT(nll_priors);
   REPORT(nll);
-  if(family == 0) {
-    REPORT(reportpolygonsd);
-  }
-  
+
   return nll;
 }
+
